0c361693501da7f3e8346dccfc2a5264f95de6d6
Daniel Bradberry <daniel@danielbradberry.com>
2020-04-14 21:45:18 +0100

    Fix various migrations errors

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="limecore-core-di",
-    version="0.0.1",
+    version="0.1.0",
 
     description="limecore: Core DI",
     long_description=long_description,
